aacb36d10053c2abe8170eee6bde5808d5297ab3
Sam Raab <raabsm@gmail.com>
2019-06-11 18:21:52 -0400

    first commit test

---
+++ WebAppTamid.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 23:54:00 2019
+
+@author: samraab
+"""
+
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world my name is sam")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8889)
+    tornado.ioloop.IOLoop.current().start()
