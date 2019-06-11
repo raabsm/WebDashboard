93bd7914bbee874da356846f924c13b1015fcb00
Sam Raab <raabsm@gmail.com>
2019-06-11 19:09:35 -0400

    added html mainPage

--- WebAppTamid.py
+++ WebAppTamid.py
@@ -11,7 +11,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world my name is sam")
+        self.render('mainPage.html')
 
 def make_app():
     return tornado.web.Application([
