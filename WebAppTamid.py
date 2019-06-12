4b0357766d09421c91b1eefd79dbd29c4ddc186c
Sam Raab <raabsm@gmail.com>
2019-06-12 00:38:37 -0400

    successfully read city name from button click

--- WebAppTamid.py
+++ WebAppTamid.py
@@ -5,13 +5,19 @@ Created on Mon Jun  3 23:54:00 2019
 
 @author: samraab
 """
-
+import requests
 import tornado.ioloop
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('mainPage.html')
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        self.write("You wrote " + self.get_body_argument("weather_city"))
+        print(self.get_body_argument("weather_city"))
+
+
 
 def make_app():
     return tornado.web.Application([
