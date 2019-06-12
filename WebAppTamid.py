ca8cfe31b1de00df82f5d4172ce684bbf9f78d06
Sam Raab <raabsm@gmail.com>
2019-06-12 15:54:49 -0400

    prints out to terminal weather temperature in inputted city

--- WebAppTamid.py
+++ WebAppTamid.py
@@ -27,11 +27,15 @@ class MainHandler(tornado.web.RequestHandler):
         message = self.get_body_argument("weather_city")
         r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + message + "&appid=" + WEATHER_API_KEY) 
         data = r.json()
-        print(data)
-        locationUrlFromLatLong = "https://developers.zomato.com/api/v2.1/cities?lat=28&lon=77"
+        temp = data['main']['temp']
+        c = temp - 273
+        f = ((9 * c) / 5) + 32
+        print("temp", f)
+        locationUrlFromLatLong = "https://developers.zomato.com/api/v2.1/geocode?lat=40.74&lon=-74.17"
         header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": "109136773c4244bb66745f4db5d67320"}
         response = requests.get(locationUrlFromLatLong, headers=header)
-        pprint(response.json())
+       # print("json response for Zomato:")
+       # pprint(response.json())
 
 def make_app():
     return tornado.web.Application([
