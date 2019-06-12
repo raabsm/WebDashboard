d721d8e8c17c378f35acd4b84bfe08cd4b6e2ef2
Sam Raab <raabsm@gmail.com>
2019-06-12 12:52:26 -0400

    successfully got zomato api to work, still need to sort through all the data

--- WebAppTamid.py
+++ WebAppTamid.py
@@ -8,8 +8,15 @@ Created on Mon Jun  3 23:54:00 2019
 import requests
 import tornado.ioloop
 import tornado.web
+import requests
+from pprint import pprint
+
 
 WEATHER_API_KEY = "119f4ed0b5ca20d098497b54a430a6c3"
+
+config={
+  "user_key":"109136773c4244bb66745f4db5d67320"
+  }
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -21,7 +28,10 @@ class MainHandler(tornado.web.RequestHandler):
         r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + message + "&appid=" + WEATHER_API_KEY) 
         data = r.json()
         print(data)
-
+        locationUrlFromLatLong = "https://developers.zomato.com/api/v2.1/cities?lat=28&lon=77"
+        header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": "109136773c4244bb66745f4db5d67320"}
+        response = requests.get(locationUrlFromLatLong, headers=header)
+        pprint(response.json())
 
 def make_app():
     return tornado.web.Application([
