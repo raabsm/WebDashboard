15f8190b4979c345bf9f92b7f9adcc9b268163a0
Sam Raab <raabsm@gmail.com>
2019-06-26 11:06:49 +0300

    successfully connected to mongoDB server and compass

---
+++ MongoDBTest.py
@@ -0,0 +1,11 @@
+import pymongo
+
+connection = pymongo.MongoClient('localhost', 27017)
+
+database = connection['mydb_01']
+
+collection = database['mycol_01']
+
+data = {'Name': "Sam"}
+
+collection.insert_one(data)
